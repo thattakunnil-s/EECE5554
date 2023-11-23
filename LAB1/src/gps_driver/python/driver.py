77a0471e4858f43e7f7670351a77e535c67162cf
thattakunnil-s <thattakunnil.s@northeastern.edu>
2023-11-23 15:53:26 -0500

    Updated driver code with minor changes

--- LAB1/src/gps_driver/python/driver.py
+++ LAB1/src/gps_driver/python/driver.py
@@ -21,31 +21,42 @@ def read(port):
 	#rospy.logwarn("GPS: No data")
      data_split = data_str.split(',')
      if '$GPGGA' in data_split[0]:
+     
+        time_str = data_split[1]
+        hours = int(time_str[:2])
+        minutes = int(time_str[2:4])
+        seconds = int(time_str[4:6])
+        milliseconds = int(time_str.split('.')[1])
+
+        time_stamp_sec = hours * 3600 + minutes * 60 + seconds
+        time_stamp_nsec = milliseconds * 1000000
+
         lat = float(data_split[2])
         lat_direction = data_split[3]
-        latitude = math.trunc(lat/100) + math.trunc((lat/100 - math.trunc(lat/100))*100)/60 + (lat - math.trunc(lat))*100/3600
+        latitude = math.trunc(lat//100) + (lat%100)/60
         
-        lng = float(data_split[4])
+        long = float(data_split[4])
         long_direction = data_split[5]
-        longitude = math.trunc(lng/100) + math.trunc((lng/100 - math.trunc(lng/100))*100)/60 + (lng - math.trunc(lng))*100/3600
+        longitude = math.trunc(long//100) + (long%100)/60
         
         altitude = float(data_split[9])
         hdop = float(data_split[8])
         
         if lat_direction == 'S':
            latitude *= -1
-        if lng_direction == 'W':
+        if long_direction == 'W':
            longitude *= -1
            
         msg = gps_msg()
         msg.Header = Header()
         msg.Header.frame_id = "GPS1_Frame"
+        msg.Header.stamp.secs = time_stamp_sec  
+        msg.Header.stamp.nsecs = time_stamp_nsec 
         msg.Latitude = latitude
         msg.Longitude = longitude
         msg.Altitude = altitude
         msg.HDOP = hdop
         msg.UTM_easting, msg.UTM_northing, msg.Zone, msg.Letter = utm.from_latlon(latitude, longitude)
-	print(msg)
         publisher.publish(msg)
 
 if __name__ == '__main__':
@@ -53,6 +64,3 @@ if __name__ == '__main__':
      read(port1)
   except rospy.ROSInterruptException:
      pass
-
-
-
